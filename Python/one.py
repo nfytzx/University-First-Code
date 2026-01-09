06720a5e9a87db47955970a9f1b00dc6747945fc
nfytzx <tzx071125@qq.com>
2026-01-09 11:46:47 +0800

    4

--- Python/one.py
+++ Python/one.py
@@ -1,1 +1,9 @@
-print('hellow world')
+num = 1
+count = 0
+print("1到20之间的偶数：")
+while num <= 20:
+    if num % 2 == 0:
+        print(num, end=" ")
+        count += 1
+    num += 1
+print(f"总共找到了 {count} 个偶数")
